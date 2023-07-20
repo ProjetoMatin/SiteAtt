1b13f87f323af36beda6e0010820d1077c48a92f
Mat-in <repositoriomatin@gmail.com>
2023-07-20 12:57:44 -0300

    Carrinho
    Carrinho 90% feito

---
+++ TCC/PAGINAS/PAGS-REP/header.php
@@ -0,0 +1,80 @@
+<link rel="stylesheet" href="../../STYLES/basico.css">
+<header>
+    <div class='logodiv'>
+        <a href="../PRINCIPAL/index.php">
+            <figure>
+                <img src='../../IMG/logo.png' alt=''>
+            </figure>
+            <h1>Mat-in</h1>
+        </a>
+    </div>
+
+    <div class='pesquisadiv'>
+
+        <select name='pesquisaFiltro' id='pesquisaFiltro'>
+            <option value='todos'>Todos</option>
+            <option value='op1'>Op1</option>
+            <option value='op2'>Op2</option>
+            <option value='op3'>Op3</option>
+            <option value='op4'>Op4</option>
+        </select>
+
+        <input type='search' name='pesquisar' id='pesquisar' placeholder='Buscar produtos, marcas e muito mais...'>
+
+        <figure>
+            <img src='../../IMG/search-icon-vetor-branco.png' alt=''>
+        </figure>
+    </div>
+
+    <div class='logindiv'>
+
+        <figure id='cart-btn'>
+            <a href='../PRINCIPAL/carrinho.php'>
+                <img src='../../IMG/cart.png' alt=''>
+            </a>
+        </figure>
+
+        <div class='loginbtn'>
+            <figure>
+                <a href='#'>
+                    <img src='../../IMG/guest-vetor-branco.png' alt=''>
+                </a>
+            </figure>
+            <a href='#'>Login</a>
+        </div>
+    </div>
+</header>
+
+<aside>
+    <div class='adminbtn'>
+        <figure>
+            <img src='../../IMG/admin-icon-branco.png' alt=''>
+        </figure>
+        <a href='#'>
+            <p>Admin</p>
+        </a>
+    </div>
+
+    <nav>
+        <ul>
+            <li><a href='#'>Categorias</a></li>
+            <li><a href='#'><img src='../../IMG/estrela-branco.png' alt=''>Favoritos</a></li>
+            <li><a href='#'>Histórico</a></li>
+            <li><a href='#'>Contato</a></li>
+        </ul>
+    </nav>
+
+    <div class='cepbtn'>
+        <div class='divcpf'>
+            <a href='#'>
+                <p>Informe seu</p>
+            </a>
+            <a href='#' id='CEPletra'>
+                <p>CEP</p>
+            </a>
+        </div>
+        <figure>
+            <a href='#'><img src='../../IMG/cep-icon-vetor-branco.png' alt=''></a>
+        </figure>
+    </div>
+</aside>
---
+++ TCC/PAGINAS/PRINCIPAL/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+
+<!--
+
+    FAVOR DEIXAR O NOME DE TODAS AS VARIÁVEIS, CLASSES, ID'S E NAMES EM PORTUGUES!
+
+-->
+
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mat-in</title>
+    <link rel="shortcut icon" href="../../STYLES/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="../../STYLES/ESPECIFICO/home.css">
+</head>
+
+<body>
+
+    <?php
+        //O CSS GERAL VAI ESTAR AQUI DENTRO DESSE HEADER.PHP, EU FIZ DESSE JEITO PARA NÃO TER QUE FICAR REPETINDO LINHAS DE CÓDIGO, SE MUDAR ALGUMA COISA DESSE HEADER.PHP VAI MUDAR PARA TODAS AS PAGINAS QUE DÃO REQUIRE NESSE ARQUIVO.
+        
+        require_once '../PAGS-REP/header.php';
+    ?>
+
+    <main>
+        <div class="carrosselbg">
+            <img src="../../IMG/pexels-oleksandr-pidvalnyi-321542.jpg" alt="">
+
+            <div class="carrosselitens">
+                <h1>Produtos que possam ser do seu interesse</h1>
+            </div>
+        </div>
+
+        <!--Não sei fazer carrossel-->
+
+        <h1 class="cath1">Categorias mais buscadas</h1>
+        <section>
+
+            <div class="catitem">
+
+                <figure>
+                    <a href="#"><img src="../../IMG/sementes-salvas.jpg" alt=""></a>
+                </figure>
+
+                <div class="infoitem">
+                    <h1><a href="#">Sementes</a></h1>
+                    <p><a href="#">160.000 vendidos nesta ultima semana!</a></p>
+                    <input type="button" value="Saiba mais">
+                </div>
+
+            </div>
+
+            <div class="catitem">
+                <figure>
+                    <a href="#"><img src="../../IMG/nutritious-breakfast-based-milk-1536x1024.jpg" alt=""></a>
+                </figure>
+
+                <div class="infoitem">
+                    <h1><a href="#">Laticinios</a></h1>
+                    <p><a href="#">160.000 vendidos nesta ultima semana!</a></p>
+                    <input type="button" value="Saiba mais">
+                </div>
+
+            </div>
+
+            <div class="catitem">
+                <figure>
+                    <a href="#"><img src="../../IMG/97f9675f-8982-47ab-84ca-64782e01187c.jpeg" alt=""></a>
+                </figure>
+
+                <div class="infoitem">
+                    <h1><a href="#">Hortifruti</a></h1>
+                    <p><a href="#">160.000 vendidos nesta ultima semana!</a></p>
+                    <input type="button" value="Saiba mais">
+                </div>
+
+            </div>
+        </section>
+
+    </main>
+
+    <footer id="tipo1">
+        <h1>Deseja ver recomendações personalizadas?</h1>
+
+        <input type="button" value="Faça seu login">
+
+        <p>Cliente novo? <strong><a href="#">Comece aqui</a></strong></p>
+    </footer>
+
+    <script src="../../JAVASCRIPT/script.js"></script>
+</body>
+
+</html>
