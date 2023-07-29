b3627a3d3bfca5f4d4b910b01189a4ef99d255a8
Mat-in <repositoriomatin@gmail.com>
2023-07-29 11:37:39 -0300

    Finalizada a primeira parte de login

--- TCC/PAGINAS/PRINCIPAL/login.php
+++ TCC/PAGINAS/PRINCIPAL/login.php
@@ -32,10 +32,10 @@
                         <i class="material-icons">mail</i>
                         <input type="email" name="email" id="email" placeholder="Digite seu e-mail..." class="formInput" maxlength="100">
                     </div>
-                    <label for="email">Senha</label>
+                    <label for="senha">Senha</label>
                     <div class="campo">
                         <i class="material-icons">vpn_key</i>
-                        <input type="password" name="email" id="email" placeholder="Digite sua senha..." class="formInput" autocomplete="on" >
+                        <input type="password" name="senha" id="senha" placeholder="Digite sua senha..." class="formInput" autocomplete="on">
                     </div>
                     <div class="naoCad">
                         <p>Não possui cadastro? <a href="cadastro.php">Cadastre-se</a></p>
