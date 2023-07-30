f02c9cabee8ab7995f1055cafc5ffb70d6b84c99
Mat-in <repositoriomatin@gmail.com>
2023-07-29 22:18:16 -0300

    Update teste.php

--- TCC/PAGINAS/PRINCIPAL/teste.php
+++ TCC/PAGINAS/PRINCIPAL/teste.php
@@ -8,23 +8,23 @@
             display: flex;
             flex-wrap: wrap;
             overflow: hidden; /* Esconde o conteúdo que ultrapassar o tamanho do carousel */
-}
+        }
 
-.card {
-  width: 30%; /* Ajuste o tamanho dos cards conforme necessário */
-  border: 1px solid #ccc;
-  margin-bottom: 10px;
-  padding: 10px;
-  transition: transform 0.3s ease-in-out; /* Adiciona uma transição de animação */
-}
+        .card {
+        width: 30%; /* Ajuste o tamanho dos cards conforme necessário */
+        border: 1px solid #ccc;
+        margin-bottom: 10px;
+        padding: 10px;
+        transition: transform 0.3s ease-in-out; /* Adiciona uma transição de animação */
+        }
 
-.show {
-  transform: translateX(0); /* Mostra o card (translação de 0) */
-}
+        .show {
+        transform: translateX(0); /* Mostra o card (translação de 0) */
+        }
 
-.hide {
-  transform: translateX(-100%); /* Esconde o card (translação para a esquerda) */
-}
+        .hide {
+        transform: translateX(-100%); /* Esconde o card (translação para a esquerda) */
+        }
     </style>
 </head>
 <body>
@@ -39,26 +39,6 @@
             <!-- Conteúdo do terceiro card aqui -->
         </div>
         <div class="card"> <!-- Card 3 -->
-            <!-- Conteúdo do terceiro card aqui -->
-        </div>
-        <div class="card"> <!-- Card 3 -->
-            <!-- Conteúdo do terceiro card aqui -->
-        </div>
-        <div class="card"> <!-- Card 3 -->
-            <!-- Conteúdo do terceiro card aqui -->
-        </div>
-        <div class="card"> <!-- Card 3 -->
-            <!-- Conteúdo do terceiro card aqui -->
-        </div>
-        <div class="card"> <!-- Card 3 -->
-            <!-- Conteúdo do terceiro card aqui -->
-        </div>
-        <div class="card"> <!-- Card 3 -->
-            <!-- Conteúdo do terceiro card aqui -->
-        </div>
-        <div class="card"> <!-- Card 3 -->
-            <!-- Conteúdo do terceiro card aqui -->
-        </div>
         <button id="btnPrev">Anterior</button>
         <button id="btnNext">Próximo</button>
     </div>
