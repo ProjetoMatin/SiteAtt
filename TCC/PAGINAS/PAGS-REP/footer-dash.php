27302475097992fdcdc7ab6150ec6ca2bee9289d
Yhureei <repositoriomatin@gmail.com>
2023-09-25 22:12:48 -0300

    Muitas mudanças - Yhureei

--- TCC/PAGINAS/PAGS-REP/footer-dash.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" href="../../STYLES/GERAL/footer-dash.css">
-<link rel="stylesheet" href="../../STYLES/GERAL/basico.css">
-<footer>
-    <h1>Mat-in &copy; 2023 - RJ</h1>
-</footer>
--- TCC/PAGINAS/PAGS-REP/footerVers1.php
+++
@@ -1,6 +0,0 @@
-<link rel="stylesheet" href="../../STYLES/GERAL/footerVers1.css">
-<footer>
-    <p>Deseja procurar mais produtos?</p>
-
-    <a href="../PRINCIPAL/index.php"><input type="button" value="Voltar à página inicial"></a>
-</footer>
