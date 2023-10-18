503d66c713b4ddd748b0d513f8e983ce5bf02978
Yhureei <repositoriomatin@gmail.com>
2023-10-18 13:41:39 -0300

    mudança dashboard - yuri

--- TCC/PAGINAS/DASHBOARD/dash_ini.php
+++ TCC/PAGINAS/DASHBOARD/dash_ini.php
@@ -65,92 +65,100 @@
         </div>
     </div>
     <div class="listas">
-        <div class="listUsu">
-            <h5>Últimos Usuários Cadastrados</h5>
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">Nº</th>
-                        <th scope="col">Nome</th>
-                        <th scope="col">Situação</th>
-                        <th scope="col">CNPJ/CPF</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
+        <div class="row">
+            <div class="col-md-6">
 
-                    $selectP = "SELECT * FROM usuario ORDER BY idUsu DESC LIMIT 5";
-                    $selectP = $cx->prepare($selectP);
-                    $selectP->setFetchMode(PDO::FETCH_ASSOC);
-                    $selectP->execute();
-                    $total = $selectP->rowCount();
 
-                    if ($total == 0) {
-                        echo "<p class='bloqueado'>Não há registros no momento...</p>";
-                    } else {
-                        while ($dados = $selectP->fetch()) {
-                            echo "<tr>";
-                            echo "<td scope='row'>{$dados['idUsu']}</td>";
-                            echo "<td>{$dados['nomeUsu']}</td>";
-                            if ($dados['ativo'] == 1) {
-                                echo "<td class='ativo'>Ativo</td>";
+                <div class="listUsu">
+                    <h5>Últimos Usuários Cadastrados</h5>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">Nº</th>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Situação</th>
+                                <th scope="col">CNPJ/CPF</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+
+                            $selectP = "SELECT * FROM usuario ORDER BY idUsu DESC LIMIT 5";
+                            $selectP = $cx->prepare($selectP);
+                            $selectP->setFetchMode(PDO::FETCH_ASSOC);
+                            $selectP->execute();
+                            $total = $selectP->rowCount();
+
+                            if ($total == 0) {
+                                echo "<p class='bloqueado'>Não há registros no momento...</p>";
                             } else {
-                                echo "<td class='bloqueado'>Bloqueado</td>";
+                                while ($dados = $selectP->fetch()) {
+                                    echo "<tr>";
+                                    echo "<td scope='row'>{$dados['idUsu']}</td>";
+                                    echo "<td>{$dados['nomeUsu']}</td>";
+                                    if ($dados['ativo'] == 1) {
+                                        echo "<td class='ativo'>Ativo</td>";
+                                    } else {
+                                        echo "<td class='bloqueado'>Bloqueado</td>";
+                                    }
+                                    echo "<td>{$dados['NRCIR']} - {$dados['TCIR']}</td>";
+                                    echo "</tr>";
+                                }
                             }
-                            echo "<td>{$dados['NRCIR']} - {$dados['TCIR']}</td>";
-                            echo "</tr>";
-                        }
-                    }
 
 
-                    ?>
-                </tbody>
-            </table>
-        </div>
-        <div class="prodlist">
-            <h5>Últimos Produtos Comprados</h5>
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">Nº</th>
-                        <th scope="col">Nome</th>
-                        <th scope="col">Situação</th>
-                        <th scope="col">Preço</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td scope="row">1</td>
-                        <td>Banana Prata</td>
-                        <td class="emProc">Em processamento</td>
-                        <td>R$20,90</td>
-                    </tr>
-                    <tr>
-                        <td scope="row">2</td>
-                        <td>Maracujá</td>
-                        <td class="emProc">Em processamento</td>
-                        <td>R$15,20</td>
-                    </tr>
-                    <tr>
-                        <td scope="row">3</td>
-                        <td>Maçã</td>
-                        <td class="Desistencia">Desistencia</td>
-                        <td>R$10,00</td>
-                    </tr>
-                    <tr>
-                        <td scope="row">4</td>
-                        <td>Kiwí</td>
-                        <td class="Desistencia">Desistencia</td>
-                        <td>R$10,00</td>
-                    </tr>
-                    <tr>
-                        <td scope="row">5</td>
-                        <td>Manga</td>
-                        <td class="Desistencia">Desistencia</td>
-                        <td>R$10,00</td>
-                    </tr>
-                </tbody>
-            </table>
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="prodlist">
+                    <h5>Últimos Produtos Comprados</h5>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">Nº</th>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Situação</th>
+                                <th scope="col">Preço</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td scope="row">1</td>
+                                <td>Banana Prata</td>
+                                <td class="emProc">Em processamento</td>
+                                <td>R$20,90</td>
+                            </tr>
+                            <tr>
+                                <td scope="row">2</td>
+                                <td>Maracujá</td>
+                                <td class="emProc">Em processamento</td>
+                                <td>R$15,20</td>
+                            </tr>
+                            <tr>
+                                <td scope="row">3</td>
+                                <td>Maçã</td>
+                                <td class="Desistencia">Desistencia</td>
+                                <td>R$10,00</td>
+                            </tr>
+                            <tr>
+                                <td scope="row">4</td>
+                                <td>Kiwí</td>
+                                <td class="Desistencia">Desistencia</td>
+                                <td>R$10,00</td>
+                            </tr>
+                            <tr>
+                                <td scope="row">5</td>
+                                <td>Manga</td>
+                                <td class="Desistencia">Desistencia</td>
+                                <td>R$10,00</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 </div>
