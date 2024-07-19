e9f267770e874e0f0eb91546c4f5cc7d6f35abc3
Yhureei <128598080+ProjetoMatin@users.noreply.github.com>
2024-07-19 15:54:31 -0300

    SQL novo - Y.H

--- MatinV2.0/DOCS/SQL/matin.sql
+++ MatinV2.0/DOCS/SQL/matin.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 05-Jul-2024 às 03:28
+-- Tempo de geração: 19/07/2024 às 20:54
 -- Versão do servidor: 10.4.32-MariaDB
--- versão do PHP: 8.2.12
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `avaliacao`
+-- Estrutura para tabela `avaliacao`
 --
 
 CREATE TABLE `avaliacao` (
@@ -42,26 +42,30 @@ CREATE TABLE `avaliacao` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `carrinho`
+-- Estrutura para tabela `carrinho`
 --
 
 CREATE TABLE `carrinho` (
-  `idCarrinho` int(11) NOT NULL
+  `idCarrinho` int(11) NOT NULL,
+  `idUsu` int(11) NOT NULL,
+  `idProduto` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Extraindo dados da tabela `carrinho`
+-- Despejando dados para a tabela `carrinho`
 --
 
-INSERT INTO `carrinho` (`idCarrinho`) VALUES
-(1),
-(2),
-(3);
+INSERT INTO `carrinho` (`idCarrinho`, `idUsu`, `idProduto`) VALUES
+(4, 8, 6),
+(5, 1, 3),
+(6, 8, 2),
+(7, 8, 3),
+(8, 2, 2);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `categoria`
+-- Estrutura para tabela `categoria`
 --
 
 CREATE TABLE `categoria` (
@@ -73,17 +77,17 @@ CREATE TABLE `categoria` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Extraindo dados da tabela `categoria`
+-- Despejando dados para a tabela `categoria`
 --
 
 INSERT INTO `categoria` (`idCategoria`, `nome_cat`, `img_cat`, `desc_cat`, `qnt_vis`) VALUES
-(1, 'Sementes', 'sem_foto.png', 'Sementes de qualidade para agricultura, jardinagem e conservação.', 176),
-(2, 'Laticínios ', 'sem_foto.png', 'Laticinios novos', 30);
+(1, 'Sementes', 'sementes.png', 'Sementes de qualidade para agricultura, jardinagem e conservação.', 176),
+(2, 'Laticínios ', 'laticinios.png', 'Laticinios novos', 30);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `cria`
+-- Estrutura para tabela `cria`
 --
 
 CREATE TABLE `cria` (
@@ -93,37 +97,51 @@ CREATE TABLE `cria` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Extraindo dados da tabela `cria`
+-- Despejando dados para a tabela `cria`
 --
 
 INSERT INTO `cria` (`idCria`, `idUsu`, `idProduto`) VALUES
 (1, 1, 2),
 (4, 1, 2),
-(5, 2, 2);
+(5, 2, 2),
+(6, 8, 7),
+(7, 9, 5),
+(8, 1, 6);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `favoritos`
+-- Estrutura para tabela `favoritos`
 --
 
 CREATE TABLE `favoritos` (
-  `idFavoritos` int(11) NOT NULL
+  `idFavoritos` int(11) NOT NULL,
+  `idProduto` int(11) NOT NULL,
+  `idUsu` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Extraindo dados da tabela `favoritos`
+-- Despejando dados para a tabela `favoritos`
 --
 
-INSERT INTO `favoritos` (`idFavoritos`) VALUES
-(1),
-(2),
-(3);
+INSERT INTO `favoritos` (`idFavoritos`, `idProduto`, `idUsu`) VALUES
+(4, 3, 8);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `local`
+-- Estrutura para tabela `fotos`
+--
+
+CREATE TABLE `fotos` (
+  `idFoto` int(11) NOT NULL,
+  `fotoNome` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `local`
 --
 
 CREATE TABLE `local` (
@@ -135,10 +153,11 @@ CREATE TABLE `local` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Extraindo dados da tabela `local`
+-- Despejando dados para a tabela `local`
 --
 
 INSERT INTO `local` (`CEP`, `Logradouro`, `Bairro`, `Cidade`, `UF`) VALUES
+(22745040, 'Rua Guanhães', 'Freguesia (Jacarepaguá)', 'Rio de Janeiro', 'RJ'),
 (26083160, 'Rua Abirú', 'Rodilândia', 'Nova Iguaçu', 'RJ'),
 (26311430, 'Rua Maria Alves', 'Nossa Senhora de Fátima', 'Queimados', 'RJ'),
 (26376100, 'Rua Camboatá', 'Cidade Jardim Cabuçu', 'Queimados', 'RJ'),
@@ -147,7 +166,7 @@ INSERT INTO `local` (`CEP`, `Logradouro`, `Bairro`, `Cidade`, `UF`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `npedido`
+-- Estrutura para tabela `npedido`
 --
 
 CREATE TABLE `npedido` (
@@ -162,7 +181,7 @@ CREATE TABLE `npedido` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Extraindo dados da tabela `npedido`
+-- Despejando dados para a tabela `npedido`
 --
 
 INSERT INTO `npedido` (`idNPedido`, `situacao`, `data_criacao`, `qnt_pedida`, `tipoqnt_ped`, `idProduto`, `idUsuComprador`, `idUsuVendedor`) VALUES
@@ -176,7 +195,7 @@ INSERT INTO `npedido` (`idNPedido`, `situacao`, `data_criacao`, `qnt_pedida`, `t
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `pergunta`
+-- Estrutura para tabela `pergunta`
 --
 
 CREATE TABLE `pergunta` (
@@ -185,7 +204,7 @@ CREATE TABLE `pergunta` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Extraindo dados da tabela `pergunta`
+-- Despejando dados para a tabela `pergunta`
 --
 
 INSERT INTO `pergunta` (`idPergunta`, `desc_perg`) VALUES
@@ -194,7 +213,7 @@ INSERT INTO `pergunta` (`idPergunta`, `desc_perg`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `produto`
+-- Estrutura para tabela `produto`
 --
 
 CREATE TABLE `produto` (
@@ -207,23 +226,35 @@ CREATE TABLE `produto` (
   `qnt_vendas` int(11) NOT NULL,
   `promocao` int(11) DEFAULT NULL,
   `parcela` int(11) DEFAULT NULL,
-  `idCarrinho` int(11) NOT NULL,
-  `idFavoritos` int(11) NOT NULL,
   `idCategoria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Extraindo dados da tabela `produto`
+-- Despejando dados para a tabela `produto`
 --
 
-INSERT INTO `produto` (`idProduto`, `nome_prod`, `qnt_prod_estoque`, `data_criacao_prod`, `preco_prod`, `fotos_prod`, `qnt_vendas`, `promocao`, `parcela`, `idCarrinho`, `idFavoritos`, `idCategoria`) VALUES
-(2, 'Maçã', 3, '2024-04-08', 12.00, 'sem_foto.png', 5, 2, 3, 1, 1, 1),
-(3, 'Leite', 5, '2024-04-03', 144.00, 'leite.jpg', 10, NULL, NULL, 2, 2, 1);
+INSERT INTO `produto` (`idProduto`, `nome_prod`, `qnt_prod_estoque`, `data_criacao_prod`, `preco_prod`, `fotos_prod`, `qnt_vendas`, `promocao`, `parcela`, `idCategoria`) VALUES
+(2, 'Maçã', 3, '2024-04-08', 12.00, 'sem_foto.png', 5, 2, 3, 1),
+(3, 'Leite', 5, '2024-04-03', 144.00, 'leite.jpg', 10, NULL, NULL, 1),
+(5, 'Leite UHT Integral', 13, '2024-07-01', 13.90, 'Leite-UHT-Integral.png', 13, 19, 2, 2),
+(6, 'Leite Condensado', 4, '2024-07-01', 19.20, 'leite_condensado.jpeg', 14, NULL, NULL, 2),
+(7, 'Carne Bovina', 41, '2024-07-22', 31.20, 'carne.png', 900, NULL, NULL, 2);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `seguidores`
+-- Estrutura para tabela `produto_has_foto`
+--
+
+CREATE TABLE `produto_has_foto` (
+  `idProd` int(11) NOT NULL,
+  `idFoto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `seguidores`
 --
 
 CREATE TABLE `seguidores` (
@@ -232,7 +263,7 @@ CREATE TABLE `seguidores` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Extraindo dados da tabela `seguidores`
+-- Despejando dados para a tabela `seguidores`
 --
 
 INSERT INTO `seguidores` (`idSeguido`, `qntSeguidor`) VALUES
@@ -242,7 +273,7 @@ INSERT INTO `seguidores` (`idSeguido`, `qntSeguidor`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `usuario`
+-- Estrutura para tabela `usuario`
 --
 
 CREATE TABLE `usuario` (
@@ -253,7 +284,7 @@ CREATE TABLE `usuario` (
   `email_usu` varchar(100) NOT NULL,
   `senha_usu` varchar(45) NOT NULL,
   `tel_usu` varchar(45) NOT NULL,
-  `fotos_usu` varchar(45) DEFAULT NULL,
+  `fotos_usu` varchar(45) NOT NULL DEFAULT 'sem_foto.png',
   `premium` enum('0','1') NOT NULL,
   `NR` varchar(45) NOT NULL,
   `comp` varchar(45) NOT NULL,
@@ -262,24 +293,28 @@ CREATE TABLE `usuario` (
   `nvl_usu` enum('A','F','C') NOT NULL DEFAULT 'C',
   `Local_CEP` int(11) NOT NULL,
   `tipoLocal` enum('casa','trabalho') NOT NULL,
-  `infoAddLocal` varchar(100) NOT NULL
+  `infoAddLocal` varchar(100) NOT NULL,
+  `qnt_vendas_usuario` int(11) NOT NULL DEFAULT 0,
+  `visto_por_ultimo` datetime DEFAULT NULL,
+  `usu_online` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Extraindo dados da tabela `usuario`
+-- Despejando dados para a tabela `usuario`
 --
 
-INSERT INTO `usuario` (`idUsu`, `nome_usu`, `ativo`, `data_criacao`, `email_usu`, `senha_usu`, `tel_usu`, `fotos_usu`, `premium`, `NR`, `comp`, `TCIR`, `NRCIR`, `nvl_usu`, `Local_CEP`, `tipoLocal`, `infoAddLocal`) VALUES
-(1, 'AdmMatin', '1', '2024-04-02', 'repositoriomatin@gmail.com', 'senhabraba1337#', '2198347-3957', NULL, '1', '209', 'apt 201', 'CNPJ', '26535178000120', 'A', 26311430, 'casa', ''),
-(2, 'Lucas', '1', '2024-04-01', 'repositorioLucas@gmail.com', '123123', '2198347-3952', NULL, '1', '12', '123', 'CPF', '30816990026', 'F', 27524302, 'casa', ''),
-(3, 'Osiris', '1', '2024-04-01', 'osirisiuri@gmail.com', 'osiris123', '212798-2165', NULL, '0', '12', '14', 'CPF', '29673467722', 'C', 26376100, 'casa', ''),
-(8, 'Yuri Esteves', '1', '2024-07-03', 'yhureei@gmail.com', '123123', '21 58192-3419', 'sem_foto.png', '0', 'S/N', 'apto 12', 'CPF', '12930192039', 'C', 26083160, 'casa', 'Em frente ao Supermercado Atacadão'),
-(9, 'Erick', '1', '2024-07-04', 'erick@gmail.com', '123123', '2198782-2663', NULL, '1', '13', 'apto 22', 'CPF', '80756463041', 'A', 26083160, 'casa', '');
+INSERT INTO `usuario` (`idUsu`, `nome_usu`, `ativo`, `data_criacao`, `email_usu`, `senha_usu`, `tel_usu`, `fotos_usu`, `premium`, `NR`, `comp`, `TCIR`, `NRCIR`, `nvl_usu`, `Local_CEP`, `tipoLocal`, `infoAddLocal`, `qnt_vendas_usuario`, `visto_por_ultimo`, `usu_online`) VALUES
+(1, 'AdmMatin', '1', '2024-04-02', 'repositoriomatin@gmail.com', 'senhabraba1337#', '2198347-3957', 'matinADM.png', '1', '209', 'apt 201', 'CNPJ', '26535178000120', 'A', 26311430, 'casa', '', 0, NULL, 0),
+(2, 'Lucas', '1', '2024-04-01', 'repositorioLucas@gmail.com', '123123', '2198347-3952', 'foto-lucas.png', '1', '12', '123', 'CPF', '30816990026', 'F', 27524302, 'casa', '', 0, '2024-07-11 23:44:44', 0),
+(3, 'Osiris', '1', '2024-04-01', 'osirisiuri@gmail.com', 'osiris123', '212798-2165', 'sem_foto.png', '0', '12', '14', 'CPF', '29673467722', 'C', 26376100, 'casa', '', 0, NULL, 0),
+(8, 'Yuri Esteves', '1', '2024-07-03', 'yhureei@gmail.com', '123123', '21 58192-3419', 'sem_foto.png', '0', 'S/N', 'apto 12', 'CPF', '12930192039', 'C', 26083160, 'casa', 'Em frente ao Supermercado Atacadão', 10000, '2024-07-11 23:56:13', 0),
+(9, 'Erick', '1', '2024-07-04', 'erick@gmail.com', '123123', '2198782-2663', 'sem_foto.png', '1', '13', 'apto 22', 'CPF', '80756463041', 'A', 26083160, 'casa', '', 0, NULL, 0),
+(10, 'Teste 04', '1', '2024-07-12', 'testelegalzao@gmail.com', '123123', '14 37418-4317', 'sem_foto.png', '0', 'S/N', 'apto 15', 'CPF', '12437148712', 'C', 22745040, 'casa', 'Perto do Shopping', 0, NULL, 0);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `usuario_has_pergunta`
+-- Estrutura para tabela `usuario_has_pergunta`
 --
 
 CREATE TABLE `usuario_has_pergunta` (
@@ -292,7 +327,7 @@ CREATE TABLE `usuario_has_pergunta` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Extraindo dados da tabela `usuario_has_pergunta`
+-- Despejando dados para a tabela `usuario_has_pergunta`
 --
 
 INSERT INTO `usuario_has_pergunta` (`Usuario_idUsu`, `Pergunta_idPergunta`, `idProduto`, `id_vendedor`, `sitPerg`, `dataPerg`) VALUES
@@ -303,7 +338,7 @@ INSERT INTO `usuario_has_pergunta` (`Usuario_idUsu`, `Pergunta_idPergunta`, `idP
 --
 
 --
--- Índices para tabela `avaliacao`
+-- Índices de tabela `avaliacao`
 --
 ALTER TABLE `avaliacao`
   ADD PRIMARY KEY (`id_aval`),
@@ -311,19 +346,21 @@ ALTER TABLE `avaliacao`
   ADD KEY `idUsu` (`idUsu`);
 
 --
--- Índices para tabela `carrinho`
+-- Índices de tabela `carrinho`
 --
 ALTER TABLE `carrinho`
-  ADD PRIMARY KEY (`idCarrinho`);
+  ADD PRIMARY KEY (`idCarrinho`),
+  ADD KEY `idProduto` (`idProduto`),
+  ADD KEY `idUsu` (`idUsu`);
 
 --
--- Índices para tabela `categoria`
+-- Índices de tabela `categoria`
 --
 ALTER TABLE `categoria`
   ADD PRIMARY KEY (`idCategoria`);
 
 --
--- Índices para tabela `cria`
+-- Índices de tabela `cria`
 --
 ALTER TABLE `cria`
   ADD PRIMARY KEY (`idCria`),
@@ -331,19 +368,27 @@ ALTER TABLE `cria`
   ADD KEY `idUsu` (`idUsu`);
 
 --
--- Índices para tabela `favoritos`
+-- Índices de tabela `favoritos`
 --
 ALTER TABLE `favoritos`
-  ADD PRIMARY KEY (`idFavoritos`);
+  ADD PRIMARY KEY (`idFavoritos`),
+  ADD KEY `idProduto` (`idProduto`),
+  ADD KEY `idUsu` (`idUsu`);
 
 --
--- Índices para tabela `local`
+-- Índices de tabela `fotos`
+--
+ALTER TABLE `fotos`
+  ADD PRIMARY KEY (`idFoto`);
+
+--
+-- Índices de tabela `local`
 --
 ALTER TABLE `local`
   ADD PRIMARY KEY (`CEP`);
 
 --
--- Índices para tabela `npedido`
+-- Índices de tabela `npedido`
 --
 ALTER TABLE `npedido`
   ADD PRIMARY KEY (`idNPedido`),
@@ -352,35 +397,40 @@ ALTER TABLE `npedido`
   ADD KEY `idUsuVendedor` (`idUsuVendedor`);
 
 --
--- Índices para tabela `pergunta`
+-- Índices de tabela `pergunta`
 --
 ALTER TABLE `pergunta`
   ADD PRIMARY KEY (`idPergunta`);
 
 --
--- Índices para tabela `produto`
+-- Índices de tabela `produto`
 --
 ALTER TABLE `produto`
   ADD PRIMARY KEY (`idProduto`),
-  ADD KEY `idCarrinho` (`idCarrinho`),
-  ADD KEY `idFavoritos` (`idFavoritos`),
   ADD KEY `idCategoria` (`idCategoria`);
 
 --
--- Índices para tabela `seguidores`
+-- Índices de tabela `produto_has_foto`
+--
+ALTER TABLE `produto_has_foto`
+  ADD KEY `idProd` (`idProd`),
+  ADD KEY `idFoto` (`idFoto`);
+
+--
+-- Índices de tabela `seguidores`
 --
 ALTER TABLE `seguidores`
   ADD PRIMARY KEY (`idSeguido`);
 
 --
--- Índices para tabela `usuario`
+-- Índices de tabela `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`idUsu`,`Local_CEP`),
   ADD KEY `fk_Usuario_Local_idx` (`Local_CEP`);
 
 --
--- Índices para tabela `usuario_has_pergunta`
+-- Índices de tabela `usuario_has_pergunta`
 --
 ALTER TABLE `usuario_has_pergunta`
   ADD PRIMARY KEY (`Usuario_idUsu`,`Pergunta_idPergunta`),
@@ -390,7 +440,7 @@ ALTER TABLE `usuario_has_pergunta`
   ADD KEY `idProduto` (`idProduto`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
@@ -403,7 +453,7 @@ ALTER TABLE `avaliacao`
 -- AUTO_INCREMENT de tabela `carrinho`
 --
 ALTER TABLE `carrinho`
-  MODIFY `idCarrinho` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idCarrinho` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de tabela `categoria`
@@ -415,13 +465,19 @@ ALTER TABLE `categoria`
 -- AUTO_INCREMENT de tabela `cria`
 --
 ALTER TABLE `cria`
-  MODIFY `idCria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `idCria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de tabela `favoritos`
 --
 ALTER TABLE `favoritos`
-  MODIFY `idFavoritos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idFavoritos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `fotos`
+--
+ALTER TABLE `fotos`
+  MODIFY `idFoto` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `local`
@@ -445,7 +501,7 @@ ALTER TABLE `pergunta`
 -- AUTO_INCREMENT de tabela `produto`
 --
 ALTER TABLE `produto`
-  MODIFY `idProduto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idProduto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de tabela `seguidores`
@@ -457,28 +513,42 @@ ALTER TABLE `seguidores`
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idUsu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idUsu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- Restrições para despejos de tabelas
+-- Restrições para tabelas despejadas
 --
 
 --
--- Limitadores para a tabela `avaliacao`
+-- Restrições para tabelas `avaliacao`
 --
 ALTER TABLE `avaliacao`
   ADD CONSTRAINT `avaliacao_ibfk_1` FOREIGN KEY (`idProduto`) REFERENCES `produto` (`idProduto`),
   ADD CONSTRAINT `avaliacao_ibfk_2` FOREIGN KEY (`idUsu`) REFERENCES `usuario` (`idUsu`);
 
 --
--- Limitadores para a tabela `cria`
+-- Restrições para tabelas `carrinho`
+--
+ALTER TABLE `carrinho`
+  ADD CONSTRAINT `carrinho_ibfk_1` FOREIGN KEY (`idProduto`) REFERENCES `produto` (`idProduto`),
+  ADD CONSTRAINT `carrinho_ibfk_2` FOREIGN KEY (`idUsu`) REFERENCES `usuario` (`idUsu`);
+
+--
+-- Restrições para tabelas `cria`
 --
 ALTER TABLE `cria`
   ADD CONSTRAINT `cria_ibfk_1` FOREIGN KEY (`idProduto`) REFERENCES `produto` (`idProduto`),
   ADD CONSTRAINT `cria_ibfk_2` FOREIGN KEY (`idUsu`) REFERENCES `usuario` (`idUsu`);
 
 --
--- Limitadores para a tabela `npedido`
+-- Restrições para tabelas `favoritos`
+--
+ALTER TABLE `favoritos`
+  ADD CONSTRAINT `favoritos_ibfk_1` FOREIGN KEY (`idProduto`) REFERENCES `produto` (`idProduto`),
+  ADD CONSTRAINT `favoritos_ibfk_2` FOREIGN KEY (`idUsu`) REFERENCES `usuario` (`idUsu`);
+
+--
+-- Restrições para tabelas `npedido`
 --
 ALTER TABLE `npedido`
   ADD CONSTRAINT `npedido_ibfk_1` FOREIGN KEY (`idProduto`) REFERENCES `produto` (`idProduto`),
@@ -486,21 +556,26 @@ ALTER TABLE `npedido`
   ADD CONSTRAINT `npedido_ibfk_3` FOREIGN KEY (`idUsuVendedor`) REFERENCES `usuario` (`idUsu`);
 
 --
--- Limitadores para a tabela `produto`
+-- Restrições para tabelas `produto`
 --
 ALTER TABLE `produto`
-  ADD CONSTRAINT `produto_ibfk_1` FOREIGN KEY (`idCarrinho`) REFERENCES `carrinho` (`idCarrinho`),
-  ADD CONSTRAINT `produto_ibfk_2` FOREIGN KEY (`idFavoritos`) REFERENCES `favoritos` (`idFavoritos`),
   ADD CONSTRAINT `produto_ibfk_3` FOREIGN KEY (`idCategoria`) REFERENCES `categoria` (`idCategoria`);
 
 --
--- Limitadores para a tabela `usuario`
+-- Restrições para tabelas `produto_has_foto`
+--
+ALTER TABLE `produto_has_foto`
+  ADD CONSTRAINT `produto_has_foto_ibfk_1` FOREIGN KEY (`idProd`) REFERENCES `produto` (`idProduto`),
+  ADD CONSTRAINT `produto_has_foto_ibfk_2` FOREIGN KEY (`idFoto`) REFERENCES `fotos` (`idFoto`);
+
+--
+-- Restrições para tabelas `usuario`
 --
 ALTER TABLE `usuario`
   ADD CONSTRAINT `fk_Usuario_Local` FOREIGN KEY (`Local_CEP`) REFERENCES `local` (`CEP`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Limitadores para a tabela `usuario_has_pergunta`
+-- Restrições para tabelas `usuario_has_pergunta`
 --
 ALTER TABLE `usuario_has_pergunta`
   ADD CONSTRAINT `fk_Usuario_has_Pergunta_Pergunta1` FOREIGN KEY (`Pergunta_idPergunta`) REFERENCES `pergunta` (`idPergunta`) ON DELETE NO ACTION ON UPDATE NO ACTION,
